59bcb9fea49f20f5c650b3c68d46bc99005b95f2
Cristina <heregacris@gmail.com>
2024-11-03 14:14:17 +0100

    El diseño de reward está hecho

---
+++ src/app/interfaces/candy.ts
@@ -0,0 +1,5 @@
+export interface Candy {
+  id: number
+  name: string
+  quantity: number
+}
---
+++ src/app/services/candies.service.ts
@@ -0,0 +1,54 @@
+import { inject, Injectable } from '@angular/core';
+import { Candy } from '../interfaces/candy';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CandiesService {
+  public candies: Candy[] = []
+  private http = inject(HttpClient);
+  public apiUrl = 'http://localhost:3000/candies';
+
+
+  
+  // public getCandies() {
+  //   let req = this.http!.get<Candy[]>(`${this.apiUrl}`);
+  //   return req.subscribe(
+  //     (data: Candy[]) => {
+  //       this.candies = data;
+  //       console.log(this.candies);
+  //     },
+  //     (error) => console.error(error)
+  //   );
+  // }
+  public createCandy(form: FormGroup): void {
+    const name = form.get('name')!.value;
+    const quantity = form.get('quantity')!.value;
+    this.createQuery(name, quantity).subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error)
+    );
+  }
+  private createQuery(name: string, quantity: number): Observable<any> {
+    return this.http.post(this.apiUrl, {
+      name: name,
+      quantity: quantity,
+    });
+  }
+
+  public getCandies() {
+    let req = this.http!.get<Candy[]>(`${this.apiUrl}`);
+    return req.subscribe(
+      (data: Candy[]) => {
+        this.candies = data;
+      },
+      (error) => console.error(error)
+    );
+  }
+
+  constructor() { }
+
+}
