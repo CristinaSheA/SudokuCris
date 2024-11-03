e4ec92d05dd42d3d316761ac066c5406e0425a70
Cristina <heregacris@gmail.com>
2024-11-03 11:20:52 +0100

    El diseño de calendar está hecho

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'adventio' title`, () => {
+  it(`should have the 'red-star' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('adventio');
+    expect(app.title).toEqual('red-star');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, adventio');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, red-star');
   });
 });
--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -13,4 +13,46 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 })
 export class CalendarComponent { 
   public year = new Date().getFullYear()
+  public day = new Date().getDate()
+
+  public month(language: string) {
+    const d = new Date();
+    let day;
+    switch (language) {
+      case 'eng':
+        const engMonth = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+        day = engMonth[d.getMonth()];
+        return day
+    
+      case 'ru':
+        const ruMonth = ["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
+        day = ruMonth[d.getMonth()];
+        return day
+    }
+    return day
+  }
+  public weekDay(language: string) {
+    const d = new Date();
+    let day;
+    switch (language) {
+      case 'eng':
+        const engWeekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+        day = engWeekday[d.getDay()];
+        return day
+    
+      case 'ru':
+        const ruWeekday = ["Воскресенье","Понедельник","Вторник","Среда","Четверг","Пятница","Суббота"];
+        day = ruWeekday[d.getDay()];
+        return day
+    }
+    return day
+  }
+  public daysToNewYear() {
+    const today = new Date();
+    const currentYear = today.getFullYear();
+    const nextYear = new Date(currentYear + 1, 0, 0)
+    const timeDifference = nextYear.getTime() - today.getTime();
+    const daysRemaining = Math.ceil(timeDifference / (1000 * 3600 * 24));
+    return daysRemaining
+  }
 }
